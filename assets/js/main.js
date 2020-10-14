0a0363daea1060090c0c4d12071f43de6443a856
Yana Manko <yanna.manko@gmail.com>
2020-10-14 20:35:48 +0300

    bombax

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,71 @@
-$(document).ready(function(){
-  $('.header-navbar__btn').on('click', function(){
-    $(this).toggleClass('header-navbar__btn--active');
-    $('.header-navbar__list').fadeToggle(500);
+(function($) {
+  let $navbar = $('#header-navbar');
+  let $btnBack2Top = $('#back2top');
+
+  /* кнопка возврата в начало страницы */
+  const back2TopOffset = 700;
+  const back2TopAnimationDuration = 500;
+  const back2TopAnimationFunction = 'swing';
+  const back2Top = (pos) =>
+    pos >= back2TopOffset
+      ? $btnBack2Top.fadeIn(500)
+      : $btnBack2Top.fadeOut(500);
+
+  $btnBack2Top.on('click', () => {
+    $('html').animate(
+      {
+        scrollTop: 0
+      },
+      back2TopAnimationDuration,
+      back2TopAnimationFunction
+    );
+    return false;
   });
-});
+  /* кнопка возврата в начало страницы */
+
+  /* добавление класса-модификатора в меню на скрол */
+  const headerClassOnScroll = 'header-navbar--scrolled';
+  const headerScrollTrigger = 100;
+
+  $(window).on('scroll', () => {
+    $(window).scrollTop() > headerScrollTrigger
+      ? $navbar.addClass(headerClassOnScroll)
+      : $navbar.removeClass(headerClassOnScroll);
+    back2Top($(window).scrollTop());
+  });
+  /* добавление класса-модификатора в меню на скрол */
+
+  /* подключение и настройка внутристраничной навигации */
+  $navbar.onePageNav({
+    currentClass: 'active',
+    scrollSpeed: 750,
+    easing: 'swing',
+    filter: ':not(.navbar-brand)'
+  });
+  /* подключение и настройка внутристраничной навигации */
+
+  /* закрытие меню при клике */
+  let windowWidth = $(window).width();
+
+  $(window).on('resize', () => (windowWidth = $(window).width()));
+
+  let $navbarLink = $('#header-navbar-collapse a');
+  let $navbarToggler = $('.navbar-toggler');
+  let $headerNavbarCollapse = $('#header-navbar-collapse');
+
+  $navbarLink.on('click', () => {
+    if (windowWidth < 992) {
+      $navbarToggler.addClass('collapsed');
+      $headerNavbarCollapse.removeClass('show');
+    }
+  });
+  /* закрытие меню при клике */
+
+  /* подключение и настрока плагина анимации */
+  AOS.init({
+    disable: 'mobile',
+    duration: 600,
+    easing: 'ease-in-sine'
+  });
+  /* подключение и настрока плагина анимации */
+})(jQuery);
